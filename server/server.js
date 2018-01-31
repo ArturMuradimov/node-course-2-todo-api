ea1e751739feb0cfaf109c783c61efa6a0502e4d
Artur Muradimov <artur.muradimov@aplana.com>
2018-01-31 22:18:10 +0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -23,13 +24,32 @@ app.post('/todos', (req, res) => {
   });
 });
 
-// list all todos
+// read all todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// read todo by id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  console.log('id: ', id);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+    return;
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+      return;
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
