3429a65093065e03cc230421f2a8f01b815fc4e8
Artur Muradimov <artur.muradimov@aplana.com>
2018-02-01 23:07:52 +0500

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -18,9 +18,10 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 // create todo
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -31,8 +32,10 @@ app.post('/todos', (req, res) => {
 });
 
 // read all todos
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
@@ -40,7 +43,7 @@ app.get('/todos', (req, res) => {
 });
 
 // read todo by id
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
@@ -48,7 +51,10 @@ app.get('/todos/:id', (req, res) => {
     return;
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       res.status(404).send();
       return;
@@ -58,7 +64,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 // delete todo by id
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
@@ -66,7 +72,10 @@ app.delete('/todos/:id', (req, res) => {
     return;
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       res.status(404).send();
       return;
@@ -76,7 +85,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 // update todos
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -92,7 +101,10 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {
+  Todo.findOneAndUpdate({
+    _id: id,
+    _creator: req.user._id
+  }, {
     $set: body
   }, {
     new: true
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -4,16 +4,6 @@ const jwt = require('jsonwebtoken');
 const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 123
-}];
-
 const user1Id = new ObjectID();
 const user2Id = new ObjectID();
 
@@ -33,6 +23,18 @@ const users = [{
     access: 'auth',
     token: jwt.sign({_id: user2Id.toHexString(), access: 'auth'}, 'abc123').toString()
   }]
+}];
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo',
+  _creator: user1Id
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 123,
+  _creator: user2Id
 }];
 
 const populateTodos = (done) => {
