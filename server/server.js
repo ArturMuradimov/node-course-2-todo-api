599eb2b4d3216aa5189ad04f38673cc0a539b954
Artur Muradimov <artur.muradimov@aplana.com>
2018-02-01 18:42:53 +0500

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -18,7 +19,6 @@ app.use(bodyParser.json());
 
 // create todo
 app.post('/todos', (req, res) => {
-  console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
@@ -42,7 +42,6 @@ app.get('/todos', (req, res) => {
 // read todo by id
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
-  console.log('id: ', id);
 
   if (!ObjectID.isValid(id)) {
     res.status(404).send();
@@ -61,7 +60,6 @@ app.get('/todos/:id', (req, res) => {
 // delete todo by id
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
-  console.log('id: ', id);
 
   if (!ObjectID.isValid(id)) {
     res.status(404).send();
@@ -118,9 +116,13 @@ app.post('/users', (req, res) => {
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
-    console.log(e);
     res.status(400).send(e);
   });
+});
+
+// get current user
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
